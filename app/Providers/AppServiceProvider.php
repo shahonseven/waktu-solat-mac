05294ed47bdf280318fca38893e3e4dd1782da00
Shahril Abdullah <sh@hril.dev>
2023-11-10 21:40:00 +0800

    malay translations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        app()->setlocale(option('locale', config('app.fallback_locale')));
+
+        Carbon::setLocale(option('locale', config('app.fallback_locale')));
     }
 }
